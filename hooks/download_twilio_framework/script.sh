2764fa2ee5dab596007aa8dd6481d5f421a1e66a
Virgilio T. Dato <virgiliodato@gmail.com>
2018-09-01 22:20:41 +0800

    fix: correct target directory

--- hooks/download_twilio_framework/script.sh
+++ hooks/download_twilio_framework/script.sh
@@ -4,6 +4,6 @@ mkdir plugins/cordova-plugin-twilio-video/src/frameworks/twilio/
 curl -L -s https://github.com/twilio/twilio-video-ios/releases/download/1.4.2/TwilioVideo.framework.zip --output plugins/cordova-plugin-twilio-video/src/frameworks/twilio/TwilioVideo.framework.zip
 printf "\n** download completed **"
 printf "\n** extracting **"
-unzip plugins/cordova-plugin-twilio-video/src/frameworks/twilio/TwilioVideo.framework.zip -d src/frameworks/twilio/
-mv plugins/cordova-plugin-twilio-video/src/frameworks/twilio/Build/iOS/TwilioVideo.framework src/frameworks/twilio/
+unzip plugins/cordova-plugin-twilio-video/src/frameworks/twilio/TwilioVideo.framework.zip -d plugins/cordova-plugin-twilio-video/src/frameworks/twilio/
+mv plugins/cordova-plugin-twilio-video/src/frameworks/twilio/Build/iOS/TwilioVideo.framework plugins/cordova-plugin-twilio-video/src/frameworks/twilio/
 printf "\n** extracting complete **"
