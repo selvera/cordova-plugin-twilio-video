03e3934b22a19a31e68b74aca3d44b88753f9fd2
CaptainJeff <CaptainJeff@users.noreply.github.com>
2018-09-27 10:46:04 -0400

    Update script.sh

--- hooks/download_twilio_framework/script.sh
+++ hooks/download_twilio_framework/script.sh
@@ -1,7 +1,7 @@
 printf "\n** downloading twilio video framework **"
 mkdir plugins/cordova-plugin-twilio-video/src/frameworks/
 mkdir plugins/cordova-plugin-twilio-video/src/frameworks/twilio/
-curl -L https://s3.amazonaws.com/ccr.mala/TwilioVideo.framework.zip --output plugins/cordova-plugin-twilio-video/src/frameworks/twilio/TwilioVideo.framework.zip
+curl -L https://github.com/twilio/twilio-video-ios/releases/download/1.4.2/TwilioVideo.framework.zip --output plugins/cordova-plugin-twilio-video/src/frameworks/twilio/TwilioVideo.framework.zip
 printf "\n** download completed **"
 printf "\n** extracting **"
 unzip plugins/cordova-plugin-twilio-video/src/frameworks/twilio/TwilioVideo.framework.zip -d plugins/cordova-plugin-twilio-video/src/frameworks/twilio/
